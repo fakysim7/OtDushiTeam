ee215e7617f8eff0d60f4b9c434a014dbb529262
fakysim7 <81712937+fakysim7@users.noreply.github.com>
2025-08-18 18:29:02 +0300

    Delete bot directory

--- bot/keyboards/main.py
+++
@@ -1,9 +0,0 @@
-# Файл: bot/keyboards/main.py
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def main_menu():
-    return ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text="📅 Забронировать")],
-        [KeyboardButton(text="📋 Мои брони")],
-        [KeyboardButton(text="❓ Помощь")]
-    ], resize_keyboard=True)
